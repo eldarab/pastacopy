522bf6e84ff6fb4dfec89521e45ba47ba75fde42
Eldar Abraham <eldar.abraham@gmail.com>
2025-07-15 19:30:13 +0300

    ruff

--- scripts/generate_example_notebooks.py
+++ scripts/generate_example_notebooks.py
@@ -10,7 +10,7 @@ example_notebooks = docs / EXAMPLE_NOTEBOOKS
 for path in sorted(example_notebooks.glob("*.ipynb")):
     name = path.stem.title()
     rel_path = path.relative_to(example_notebooks)
-    nav[(name, )] = rel_path.as_posix()
+    nav[(name,)] = rel_path.as_posix()
 
-with mkdocs_gen_files.open(f"{EXAMPLE_NOTEBOOKS}/SUMMARY.md", "w") as nav_file:  
+with mkdocs_gen_files.open(f"{EXAMPLE_NOTEBOOKS}/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
